8746757a168e2a2c9df51b23dfb12f87ddbd3c59
Ayoub <60711592+ayoubrs@users.noreply.github.com>
2022-04-12 14:34:00 +0000

    Add files via upload

---
+++ Aircraft.java
@@ -0,0 +1,18 @@
+
+public class Aircraft 
+{
+
+    protected long id;
+    protected String name;
+    protected Coordinates coordinates;
+    private long idCounter;
+
+    public Place(String ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public String toString() {
+        return this.ID;
+    }
+}
