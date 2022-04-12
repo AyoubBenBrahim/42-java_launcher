dc7899528d59d2b789cfb3038c90b7da22e22982
Ayoub Bouras <aybouras@student.1337.ma>
2022-04-12 18:01:17 +0000

    java

--- WeatherProvider.java
+++ WeatherProvider.java
@@ -1,3 +1,11 @@
 public class WeatherProvider{
-    
+    private WeatherProvider weatherProvider;
+    private String[] weather;
+    WeatherProvider(){}
+    public WeatherProvider getProvider() {
+        return this.weatherProvider;
+    }
+
+    public String getCurrentWeather(Coordinates coordinates){return "null";}
+
 }
